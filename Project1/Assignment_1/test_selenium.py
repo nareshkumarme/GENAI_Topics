f3a44f8ebe945687910a7f51a97a3ebbd8764a13
nareshkumarme <nareshkumar.yella@gmail.com>
2026-04-16 22:42:51 +0530

    Assignment_1

---
+++ Project1/Assignment_1/test_selenium.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.get("https://www.google.com")
+print("Page title:", driver.title)
+driver.quit()
+print("Selenium is working!")
